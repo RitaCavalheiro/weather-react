93b96e3d3be6111c23db986bf2a4716b97551a5c
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2022-01-15 18:43:34 +0000

    added git link

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p class="gitLink">
+      <a
+        href="https://github.com/RitaCavalheiro/weather-react/tree/master/src"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="gitLink"
+      >
+        Open-source code
+      </a>
+      by Rita Cavalheiro
+    </p>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
